b3f7e24755c3fdb75904b3be1cc5529ee6bf6e60
Mousaidna Rosario <74958156+mousaidnarosario@users.noreply.github.com>
2025-03-13 15:59:13 +1100

    Update return_unique_values.py
    Changes in the code

--- return_unique_values.py
+++ return_unique_values.py
@@ -4,13 +4,12 @@
 #               1. Checks list or dictionaries and and remove the duplicates 
 #               2. Returns values in the same order of the elements without the duplicates
 
-def unique(elements):
-    counter = 0
+def unique(elements=[]):
     return_values = []
     for element in elements:
         if element not in return_values:
             return_values.append(element)
-    print(return_values)
+    return return_values
 
-unique(['Mark', 'Mark', 'John', 'Anne'])
+unique([0, 1, 1, 1, 1, 1, 23, 12, 12, 391, 98])
             
